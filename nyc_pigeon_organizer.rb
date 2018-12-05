3a3ad173dea49babff183cfd65b61d0710ba3c7e
Leizl Samano <leizl.samano@gmail.com>
2018-12-05 04:32:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Fills pigeon_list with names as keys and empty hashes as values
+  # ex. {"Theo" => {}, "Peter Jr" => {}, ...}
+  pigeon_list = {}
+  data.each do |type_keys, type_value_hash|
+    type_value_hash.each do |key, names_array|
+      names_array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][type_keys] == nil
+          pigeon_list[name][type_keys] = []
+        end
+        pigeon_list[name][type_keys] << key.to_s
+      end
+    end
+  end
+  pigeon_list
 end
